32be8129e9cc76878b81707591c1c210dcdd25d4
Ren <cassandra.chen@uoit.net>
2022-02-01 02:51:12 +0000

    fix the no. 2 in exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,6 +8,8 @@ Burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: TR
 Richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: FALSE, womens_apparel: TRUE)
 Gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: TRUE, womens_apparel: FALSE)
 
+count = Store.count(:id)
+puts count
 
 # class Burnaby < ApplicationRecord
 #   annual_revenue = 300000,
